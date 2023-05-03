d043e4561b1ee1e1ead52d88bfda8297d190133d
jayhawk <jayhawk.dev@gmail.com>
2023-05-03 23:57:54 +0900

    Feat: 버튼 세팅
    main에 size박스와 const 한 번에 넣기, 버튼 만들기 등을 연습함.

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,28 @@ class App extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
+          backgroundColor: const Color(0xFF181818),
           body: Padding(
-            padding: EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               horizontal:40,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 80,
                 ),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.
-                  end,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Text(
+                        const Text(
                           'Hey, Selena',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 38,
+                            fontSize: 28,
                             fontWeight: FontWeight.w800,
                           ),
                         ),
@@ -38,11 +38,50 @@ class App extends StatelessWidget{
                           'Welcome back',
                           style: TextStyle(
                             color: Colors.white.withOpacity(0.8),
-                            fontSize: 28,
+                            fontSize: 18,
                           ),
                         ),
                       ],
                     ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text('Total Balance',
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8)),
+                ),
+                const SizedBox(height: 10,
+                ),
+                const Text('\$5 194 482',
+                  style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 40,
+                        ),
+                        child: Text('Transfer',
+                        style: TextStyle(
+                          fontSize: 18,
+                        )),
+                      ),
+                    )
                   ],
                 ),
               ],

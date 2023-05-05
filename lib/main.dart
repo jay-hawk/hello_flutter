f0dd2fdaf2e797866588eae1621822cbcc78465e
jayhawk <jayhawk.dev@gmail.com>
2023-05-05 21:52:14 +0900

    Feat: StatelessWidget Main 만들기
    flutter로 StatelessWdiget을 활용해 main화면을 만듦.
    추가적으로 Reusable을 위해 button.dart 와 currency_card.dart를 만들었다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/widgets/button.dart';
+import 'package:hello_flutter/widgets/currency_card.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const userName = 'JayHawk';
+
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: Padding(
+        backgroundColor: const Color(0xFF181818),
+        body: SingleChildScrollView(
+          child: Padding(
             padding: const EdgeInsets.symmetric(
-              horizontal:40,
+              horizontal: 20,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(
-                  height: 80,
+                  height: 40,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -27,7 +32,7 @@ class App extends StatelessWidget{
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         const Text(
-                          'Hey, Selena',
+                          'Hey, $userName',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 28,
@@ -46,16 +51,18 @@ class App extends StatelessWidget{
                   ],
                 ),
                 const SizedBox(
-                  height: 120,
+                  height: 40,
                 ),
-                Text('Total Balance',
-                style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white.withOpacity(0.8)),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white.withOpacity(0.8)),
                 ),
-                const SizedBox(height: 10,
+                const SizedBox(
+                  height: 10,
                 ),
-                const Text('\$5 194 482',
+                const Text(
+                  '\$5 194 482',
                   style: TextStyle(
                       fontSize: 48,
                       fontWeight: FontWeight.w600,
@@ -64,31 +71,75 @@ class App extends StatelessWidget{
                 const SizedBox(
                   height: 30,
                 ),
-                Row(
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(45),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 20,
-                          horizontal: 40,
-                        ),
-                        child: Text('Transfer',
-                        style: TextStyle(
-                          fontSize: 18,
-                        )),
-                      ),
-                    )
+                    Button(
+                      text: 'Transfer',
+                      bgColor: Color(0xFFF2B33A),
+                      textColor: Colors.black,
+                    ),
+                    Button(
+                      text: 'Request',
+                      bgColor: Color(0xFF1F2123),
+                      textColor: Colors.white,
+                    ),
                   ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'View All',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CurrencyCard(
+                  name: 'Euro',
+                  code: 'EUR',
+                  amount: '6 428',
+                  icon: Icons.euro_rounded,
+                  isInverted: false,
+                  order: 1,
+                ),
+                const CurrencyCard(
+                  name: 'Bitcoin',
+                  code: 'BTC',
+                  amount: '1 231',
+                  icon: Icons.currency_bitcoin,
+                  isInverted: true,
+                  order: 2,
+                ),
+                const CurrencyCard(
+                  name: 'Dollar',
+                  code: 'USD',
+                  amount: '2 128',
+                  icon: Icons.attach_money_outlined,
+                  isInverted: false,
+                  order: 3,
                 ),
               ],
             ),
-          )
+          ),
+        ),
       ),
     );
   }
 }
-
